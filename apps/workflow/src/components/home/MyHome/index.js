5515546428dd1a7242be77dc2695914cd53e08a1
Milande Teisserenc bonesteve <milandejf@gmail.com>
2021-03-07 23:59:31 +0100

    fix bugs

--- apps/workflow/src/components/home/MyHome/index.js
+++ apps/workflow/src/components/home/MyHome/index.js
@@ -13,40 +13,39 @@ import {
 
 const MyHome = ({ data }) => {
     const [openModal, setOpenModal] = useState(false);
+    const [title, setTitle] = useState(data.data.getProject.title);
 
     const change = e => {
-        data.title = e.target.value;
+        // data.title = e.target.value;
+        setTitle(e.target.value);
     };
+
     return (
         <>
             <LabelStyled htmlFor="myTitle">Choose your title</LabelStyled>
-            <InputBar
-                id="myTitle"
-                value={data.data.getProject.title}
-                onChange={change}
-            />
+            <InputBar id="myTitle" value={title} onChange={change} />
             <Container>
-                <Title>{data.data.getProject.title}</Title>
+                <Title>{title}</Title>
                 <MyContainer>
                     <ButtonStyled onClick={() => setOpenModal(!openModal)}>
                         {openModal ? 'Open the modal' : 'Close the modal'}
                     </ButtonStyled>
-                    {openModal ? (
-                        <SubMenu>
-                            <ul>
-                                <li>This is the sub menu</li>
-                                <li>This is the sub menu</li>
-                                <li>This is the sub menu</li>
-                                <li>This is the sub menu</li>
-                                <li>This is the sub menu</li>
-                                <li>This is the sub menu</li>
-                                <li>This is the sub menu</li>
-                                <li>This is the sub menu</li>
-                            </ul>
-                        </SubMenu>
-                    ) : null}
                 </MyContainer>
             </Container>
+            {openModal ? (
+                <SubMenu>
+                    <ul>
+                        <li>This is the sub menu</li>
+                        <li>This is the sub menu</li>
+                        <li>This is the sub menu</li>
+                        <li>This is the sub menu</li>
+                        <li>This is the sub menu</li>
+                        <li>This is the sub menu</li>
+                        <li>This is the sub menu</li>
+                        <li>This is the sub menu</li>
+                    </ul>
+                </SubMenu>
+            ) : null}
         </>
     );
 };
