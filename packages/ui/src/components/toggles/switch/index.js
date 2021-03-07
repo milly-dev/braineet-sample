7cbfb59a41e3e4b85feb3807d4f7440e14e3b9d0
Milande Teisserenc bonesteve <milandejf@gmail.com>
2021-03-07 20:38:20 +0100

    implement a toggle component with style

--- packages/ui/src/components/toggles/switch/index.js
+++ packages/ui/src/components/toggles/switch/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { CheckBoxWrapper, CheckBoxLabel, CheckBox } from './styles';
 
 /**
  * The `Toggle` component.
  */
 
-export const Switch = () => {
-    return <>Write your component</>;
+export const Switch = props => {
+    const { disabled } = props;
+    const { label } = props;
+
+    return (
+        <div>
+            <CheckBoxWrapper>
+                {disabled === true ? (
+                    <CheckBox id="checkbox" type="checkbox" disabled />
+                ) : (
+                    <CheckBox id="checkbox" type="checkbox" />
+                )}
+                {label}
+                <CheckBoxLabel htmlFor="checkbox" />
+            </CheckBoxWrapper>
+        </div>
+    );
 };
 
-Switch.propTypes = {};
-Switch.defaultProps = {};
+Switch.propTypes = {
+    disabled: PropTypes.bool,
+    label: PropTypes.string,
+};
+Switch.defaultProps = {
+    disabled: false,
+    label: 'This is a label',
+};
 export default Switch;
